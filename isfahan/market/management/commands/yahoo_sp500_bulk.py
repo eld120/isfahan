0fcb332da29362f77ee96472db7808d447259f74
Seyamack Afzali <seyamack@gmail.com>
2024-08-24 23:52:41 -0500

    fixed yfinance edge cases

--- isfahan/market/management/commands/yahoo_sp500_bulk.py
+++ isfahan/market/management/commands/yahoo_sp500_bulk.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 import yfinance as yf
 from django.core.management.base import BaseCommand
 
@@ -18,6 +21,7 @@ class Command(BaseCommand):
         ).to_dict()
 
         data = {}
+
         for key, val in top_100.items():
             noun, ticker = key
             if noun in ("Close", "Volume"):
@@ -32,17 +36,33 @@ class Command(BaseCommand):
                             "price" if noun == "Close" else "volume"
                         ] = value
         new_transactions = []
+        failed_transactions = []
+        fail_flag = False
         for k, v in data.items():
             ticker, timestamp = k
             form = StockPriceForm(
                 {
                     "stock": v["stock"],
-                    "price": v["price"],
+                    "price": round(v["price"], 4),
                     "date": timestamp,
                     "volume": v["volume"],
                 },
             )
             if form.is_valid():
                 new_transactions.append(StockPrice(**form.cleaned_data))
+            else:
+                fail_flag = True
+                failed_transactions.append(
+                    {
+                        "stock": v["stock"],
+                        "price": v["price"],
+                        "date": timestamp.to_pydatetime().strftime("%m/%d/%Y"),
+                        "volume": v["volume"],
+                        "erros": form.errors,
+                    },
+                )
+        if fail_flag:
+            with Path.open("failed_transactions.json", "w") as file:
+                json.dump(failed_transactions, file)
         StockPrice.objects.bulk_create(new_transactions)
         self.stdout.write(self.style.SUCCESS("Successfully created stock price data"))
